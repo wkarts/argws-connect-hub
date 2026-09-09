7eb642d7d743189fe26cb05bf27c3756fc71979e
wkarts <57051272+wkarts@users.noreply.github.com>
2026-09-09 10:27:03 -0300

    fix(auth): initialize HUB password policy on hash-backed config.x

--- config/initializers/hub_password_policy.rb
+++ config/initializers/hub_password_policy.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.x.hub.password_policy = ActiveSupport::OrderedOptions.new
-  config.x.hub.password_policy.uppercase = ENV.fetch('HUB_PASSWORD_REQUIRED_UPPERCASE', '1').to_i
-  config.x.hub.password_policy.lowercase = ENV.fetch('HUB_PASSWORD_REQUIRED_LOWERCASE', '1').to_i
-  config.x.hub.password_policy.number = ENV.fetch('HUB_PASSWORD_REQUIRED_NUMBER', '1').to_i
-  config.x.hub.password_policy.special = ENV.fetch('HUB_PASSWORD_REQUIRED_SPECIAL', '1').to_i
+  config.x[:hub] ||= ActiveSupport::HashWithIndifferentAccess.new
+  config.x[:hub][:password_policy] ||= ActiveSupport::HashWithIndifferentAccess.new
+
+  policy = config.x[:hub][:password_policy]
+  policy[:uppercase] = ENV.fetch('HUB_PASSWORD_REQUIRED_UPPERCASE', '1').to_i
+  policy[:lowercase] = ENV.fetch('HUB_PASSWORD_REQUIRED_LOWERCASE', '1').to_i
+  policy[:number] = ENV.fetch('HUB_PASSWORD_REQUIRED_NUMBER', '1').to_i
+  policy[:special] = ENV.fetch('HUB_PASSWORD_REQUIRED_SPECIAL', '1').to_i
 end
