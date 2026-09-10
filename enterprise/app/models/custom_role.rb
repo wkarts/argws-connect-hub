194cdc5462b46d6981ed7a908aa375ff007db2d8
wkarts <57051272+wkarts@users.noreply.github.com>
2026-09-10 08:25:57 -0300

    feat(enterprise): restaurar Custom Roles

---
+++ enterprise/app/models/custom_role.rb
@@ -0,0 +1,16 @@
+class CustomRole < ApplicationRecord
+  belongs_to :account
+  has_many :account_users, dependent: :nullify
+
+  PERMISSIONS = %w[
+    conversation_manage
+    conversation_unassigned_manage
+    conversation_participating_manage
+    contact_manage
+    report_manage
+    knowledge_base_manage
+  ].freeze
+
+  validates :name, presence: true
+  validates :permissions, inclusion: { in: PERMISSIONS }
+end
