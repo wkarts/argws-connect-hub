2beef30a2d9324087de4c0f8371f1aac22c47e53
wkarts <57051272+wkarts@users.noreply.github.com>
2026-09-10 06:30:57 -0300

    test(super-admin): alinhar saúde da instância em pt-BR

--- spec/controllers/super_admin/instance_statuses_controller_spec.rb
+++ spec/controllers/super_admin/instance_statuses_controller_spec.rb
@@ -12,11 +12,13 @@ RSpec.describe 'Super Admin Instance status', type: :request do
     end
 
     context 'when it is an authenticated super admin' do
-      it 'shows the instance_status page' do
+      it 'shows the localized instance status page with the current HUB version' do
         sign_in(super_admin, scope: :super_admin)
         get '/super_admin/instance_status'
+
         expect(response).to have_http_status(:success)
-        expect(response.body).to include('Hub version')
+        expect(response.body).to include('Versão do HUB')
+        expect(response.body).to include(Hub.version)
         expect(response.body).to include(GIT_HASH)
       end
     end
