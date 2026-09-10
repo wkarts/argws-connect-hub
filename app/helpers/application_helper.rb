db7fe1ecb62ef7859e91c61934d6cb9789d774c9
wkarts <57051272+wkarts@users.noreply.github.com>
2026-09-10 06:23:50 -0300

    fix(i18n): expor somente idiomas habilitados

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def available_locales_with_name
-    LANGUAGES_CONFIG.map { |_key, val| val.slice(:name, :iso_639_1_code) }
+    LanguageConfig.available_locales.map { |val| val.slice(:name, :iso_639_1_code) }
   end
 
   def feature_help_urls
