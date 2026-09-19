42862a16179f0893fa3e7978b0fbb3151a815e9c
wkarts <57051272+wkarts@users.noreply.github.com>
2026-09-18 23:17:10 -0300

    feat(contacts): aceitar data de nascimento na API

--- app/controllers/api/v1/accounts/contacts_controller.rb
+++ app/controllers/api/v1/accounts/contacts_controller.rb
@@ -159,7 +159,7 @@ class Api::V1::Accounts::ContactsController < Api::V1::Accounts::BaseController
   end
 
   def permitted_params
-    params.permit(:name, :identifier, :email, :phone_number, :avatar, :blocked, :avatar_url, additional_attributes: {}, custom_attributes: {})
+    params.permit(:name, :identifier, :email, :phone_number, :date_of_birth, :avatar, :blocked, :avatar_url, additional_attributes: {}, custom_attributes: {})
   end
 
   def contact_custom_attributes
