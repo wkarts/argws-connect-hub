673e889fae61e6c6d12d18bf6fe85d10eecd5db4
wkarts <57051272+wkarts@users.noreply.github.com>
2026-09-20 11:45:45 -0300

    feat(link-preview): expor preview account-scoped

---
+++ app/controllers/api/v1/accounts/link_previews_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Api::V1::Accounts::LinkPreviewsController < Api::V1::Accounts::BaseController
+  def show
+    preview = LinkPreviews::Fetcher.new(params[:url]).perform
+    render json: { preview: preview }
+  rescue LinkPreviews::Fetcher::Error => error
+    Rails.logger.debug("[HUB link preview] #{error.message}")
+    render json: { preview: nil }, status: :ok
+  end
+end
